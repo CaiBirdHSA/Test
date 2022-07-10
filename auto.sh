0d4675303120c6ed936a341967323f1d9a8660e4
zh <1605879419@qq.com>
2022-07-10 22:04:10 +0800

    added files

---
+++ auto.sh
@@ -0,0 +1,8 @@
+git config --global user.email "1605879419@qq.com"
+git config --global user.name "zh"
+
+# git clone https://github.com/CaiBirdHSA/Test
+
+git add *
+git commit -m "added files"
+git push
