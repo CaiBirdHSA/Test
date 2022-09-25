99e42644c39d46e835a63640c004acd3b95cd8f4
caibirdhu-linux <1605879419@qq.com>
2022-09-25 10:26:44 +0800

    added files

--- auto.sh
+++ auto.sh
@@ -4,7 +4,7 @@
 # git clone https://github.com/CaiBirdHSA/Test
 
 
-git remote set-url origin https://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@github.com/CaiBirdHSA/Web_spider_visualization.git
+# git remote set-url origin https://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@github.com/CaiBirdHSA/Web_spider_visualization.git
 
 git add *
 git commit -m "added files"
