a224f011204c252b3122e52498b66624e5332891
CaiBirdHSA <1605879419@qq.com>
2022-09-25 10:29:33 +0800

    added files

--- auto.sh
+++ auto.sh
@@ -1,10 +1,13 @@
-git config --global user.email "1605879419@qq.com"
-git config --global user.name "CaiBirdHSA"
+#git config --global user.email "1605879419@qq.com"
+#git config --global user.name "CaiBirdHSA"
 
 # git clone https://github.com/CaiBirdHSA/Test
 
 
 # git remote set-url origin https://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@github.com/CaiBirdHSA/Web_spider_visualization.git
+
+
+git remote set-url origin git@github.com:CaiBirdHSA/Web_spider_visualization.git
 
 git add *
 git commit -m "added files"
